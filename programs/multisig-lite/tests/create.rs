d03a8b3eff7f88cab711ab526867d0278fcd4869
Keith Noguchi <keith@noguchi.us>
2023-02-09 12:56:35 -0800

    fixup! 0.0.7: multisig-lite program test
    get the tx

--- programs/multisig-lite/tests/create.rs
+++ programs/multisig-lite/tests/create.rs
@@ -1,13 +1,15 @@
 //! `multisig_list::State` unit tests.
 
+use std::error::Error;
 use std::rc::Rc;
 
 use solana_program_test::ProgramTest;
+use solana_sdk::pubkey::Pubkey;
 use solana_sdk::signer::Signer;
-use solana_sdk::transaction::Transaction;
+use solana_sdk::system_program;
 
 #[tokio::test]
-async fn create() {
+async fn create() -> Result<(), Box<dyn Error>> {
     // Starts the on-chain program first.
     let (mut _banks_client, funder, _recent_blockhash) =
         ProgramTest::new("multisig_lite", multisig_lite::id(), None)
@@ -22,4 +24,23 @@ async fn create() {
         .program(multisig_lite::id());
 
     assert_eq!(funder.pubkey(), program.payer());
+
+    // Calculates the PDAs.
+    let (state_pda, _state_bump) =
+        Pubkey::find_program_address(&[b"state", funder.pubkey().as_ref()], &multisig_lite::id());
+    let (fund_pda, _fund_bump) =
+        Pubkey::find_program_address(&[b"fund", state_pda.as_ref()], &multisig_lite::id());
+
+    // Creates a transaction.
+    let _tx = program
+        .request()
+        .accounts(multisig_lite::accounts::Create {
+            funder: funder.pubkey(),
+            state: state_pda,
+            fund: fund_pda,
+            system_program: system_program::id(),
+        })
+        .transaction()?;
+
+    Ok(())
 }
