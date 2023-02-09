2648b55a8f4d07a1e9e156107737d0d5740969e7
Keith Noguchi <keith@noguchi.us>
2023-02-09 12:17:31 -0800

    fixup! fixup! 0.0.7: multisig-lite program test
    cargo fmt

--- programs/multisig-lite/tests/create.rs
+++ programs/multisig-lite/tests/create.rs
@@ -3,19 +3,16 @@
 use std::rc::Rc;
 
 use solana_program_test::ProgramTest;
-use solana_sdk::transaction::Transaction;
 use solana_sdk::signer::Signer;
+use solana_sdk::transaction::Transaction;
 
 #[tokio::test]
 async fn create() {
     // Starts the on-chain program first.
-    let (mut _banks_client, funder, _recent_blockhash) = ProgramTest::new(
-        "multisig_lite",
-        multisig_lite::id(),
-        None,
-    )
-    .start()
-    .await;
+    let (mut _banks_client, funder, _recent_blockhash) =
+        ProgramTest::new("multisig_lite", multisig_lite::id(), None)
+            .start()
+            .await;
 
     // Wrap it to be shared with anchor_client.
     let funder = Rc::new(funder);
