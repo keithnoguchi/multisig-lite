d33be67200b3cd8ab37a4ced67f9ec346c4f79b1
Keith Noguchi <keith@noguchi.us>
2023-02-10 12:18:28 -0800

    0.0.7: Use the special private key for the CI purpose
    I'm not a big fun of storing keys in the vcs but
    this is the only way I can think of, atm.

--- programs/multisig-lite/src/lib.rs
+++ programs/multisig-lite/src/lib.rs
@@ -9,7 +9,10 @@ use anchor_lang::prelude::*;
 use anchor_lang::solana_program::program::{invoke, invoke_signed};
 use anchor_lang::solana_program::system_instruction;
 
+#[cfg(not(feature = "ci"))]
 declare_id!("Ecycmji8eeggXrA3rD2cdEHpHDnP4btvVfcyTBS9cG9t");
+#[cfg(feature = "ci")]
+declare_id!("AeAQKcvUbG6LmunEAiL2Vim5dN2uL5TNwJfgsGdyroQ3");
 
 /// A multisig program specific error code.
 #[error_code]
