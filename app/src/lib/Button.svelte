b135a36af57ba403fa17812c022f188d182d909d
Keith Noguchi <keith@noguchi.us>
2023-02-16 14:16:35 -0800

    app: Add github link button

--- app/src/lib/Button.svelte
+++ app/src/lib/Button.svelte
@@ -58,7 +58,7 @@
 			background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
 		}
 		&.size-sm {
-			padding: 15px 20px;
+			padding: 5px 10px;
 		}
 		&.size-lg {
 			padding: 20px 25px;
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,10 +1,27 @@
 <script>
+	import { goto } from '$app/navigation';
 	import Button from '$lib/Button.svelte';
-	import FaWallet from 'svelte-icons/fa/FaWallet.svelte';
 	import FaCommentDollar from 'svelte-icons/fa/FaCommentDollar.svelte';
+	import FaGithub from 'svelte-icons/fa/FaGithubSquare.svelte';
+	import FaWallet from 'svelte-icons/fa/FaWallet.svelte';
+
+	let home = 'https://github.com/keithnoguchi/multisig-lite';
 </script>
 
-<Button on:click={() => alert(true)} let:isLeftHovered size="large" shadow disabled>
+<Button on:click={() => goto(home)} bgColor="purple" size="small" shadow>
+	<div style:width="16px">
+		<FaGithub />
+	</div>
+</Button>
+
+<Button
+	on:click={() => alert(true)}
+	let:isLeftHovered
+	bgColor="purple"
+	size="large"
+	shadow
+	disabled
+>
 	<div style:width="20px" slot="leftContent">
 		{#if isLeftHovered}
 			<FaCommentDollar />
