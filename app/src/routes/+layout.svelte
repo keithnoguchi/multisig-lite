489f1269f066b4622699430c157233a8ed8b09ee
Keith Noguchi <keith@noguchi.us>
2023-02-16 15:33:21 -0800

    app: Use +layout.svelte
    Nothing changes at this point, as we only have
    a single page, but it's better to have a structure
    from day one.

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { goto } from '$app/navigation';
+	import Button from '$lib/Button.svelte';
+	import FaGithub from 'svelte-icons/fa/FaGithubSquare.svelte';
+
+	let home = 'https://github.com/keithnoguchi/multisig-lite';
+</script>
+
+<header>
+	<Button on:click={() => goto(home)} bgColor="purple" size="small" shadow>
+		<div style:width="16px">
+			<FaGithub />
+		</div>
+	</Button>
+</header>
+
+<slot />
+
+<footer />
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,18 +1,8 @@
 <script>
-	import { goto } from '$app/navigation';
 	import Button from '$lib/Button.svelte';
 	import FaCommentDollar from 'svelte-icons/fa/FaCommentDollar.svelte';
-	import FaGithub from 'svelte-icons/fa/FaGithubSquare.svelte';
 	import FaWallet from 'svelte-icons/fa/FaWallet.svelte';
-
-	let home = 'https://github.com/keithnoguchi/multisig-lite';
 </script>
-
-<Button on:click={() => goto(home)} bgColor="purple" size="small" shadow>
-	<div style:width="16px">
-		<FaGithub />
-	</div>
-</Button>
 
 <Button
 	on:click={() => alert(true)}
