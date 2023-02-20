80979822f61d967c13cd8113613d42088870026d
Keith Noguchi <keith@noguchi.us>
2023-02-20 11:54:14 -0800

    app: Load the idl json file
    Let's have a anchor Program value
    to interact with the on-chain program!

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <script>
+	import { Program } from '@project-serum/anchor';
+	import { provider } from '../stores/provider';
+	import idl from '../idl/multisig_lite.json';
 </script>
+
+<h1>multisig-lite</h1>
