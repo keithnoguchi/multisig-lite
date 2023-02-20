56b95d0835fc84c111484df4d7c3d16e4fbbeea3
Keith Noguchi <keith@noguchi.us>
2023-02-20 12:36:23 -0800

    app: Construct anchor.Program
    Finally, it allocates the anchor.Program value,
    which is the interface to the on-chain program!

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,6 +2,16 @@
 	import { Program } from '@project-serum/anchor';
 	import { provider } from '../stores/provider';
 	import idl from '../idl/multisig_lite.json';
+
+	$: program = newProgram($provider);
+	function newProgram(provider) {
+		if (!idl.metadata || !provider) return undefined;
+		return new Program(idl, idl.metadata.address, provider);
+	}
 </script>
 
 <h1>multisig-lite</h1>
+
+{#if program}
+	<p>ProgramID: {program.programId}</p>
+{/if}
