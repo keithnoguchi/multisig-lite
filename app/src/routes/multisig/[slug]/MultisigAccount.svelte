887810bd49683dfa7f6fa80c86b3ed691e4ce118
Keith Noguchi <keith@noguchi.us>
2023-02-22 20:39:17 -0800

    app: Add multisig account page

---
+++ app/src/routes/multisig/[slug]/MultisigAccount.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let address;
+</script>
+
+<header>
+	<h2>Multisig Account</h2>
+
+	<p>{address}</p>
+</header>
+
+<style lang="scss">
+	header {
+		text-align: center;
+	}
+</style>
